f4ccc2ac35e8db98d496fee97484c56c9a5d62bb
ikuradon <hawaiianphoto@geekhost.net>
2026-07-18 11:22:38 +0900

    Extend timeline beneath tab bar

--- Astrenza/Sources/AstrenzaApp/Components/Timeline/TimelineFeedView.swift
+++ Astrenza/Sources/AstrenzaApp/Components/Timeline/TimelineFeedView.swift
@@ -188,6 +188,7 @@ struct TimelineFeedView: View {
             TimelineFeedCollectionView(
                 configuration: collectionConfiguration
             )
+            .ignoresSafeArea(.container, edges: .bottom)
 
             if entries.isEmpty {
                 TimelineEmptyStateView(
